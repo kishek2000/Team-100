2f5502600677b57af20155a3acf012a22c355870
Adi <kishek12@gmail.com>
2020-03-26 17:32:35 +1100

    Adding frontend basic foundation

---
+++ App/frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+// import searchIcon from "./search.png"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="myLounge-heading">
+          myLounge
+        </p>
+        <p className="myLounge-subtitle">
+          Listen. Watch. Relax.
+        </p>
+      </header>
+      <body className="App-media-options">
+      </body>
+    </div>
+  );
+}
+
+export default App;
